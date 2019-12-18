b26a5d8a90752fc156d8ebae1273e978d5bb203a
jogyuchang <whrbckd23@naver.com>
2019-12-18 15:04:30 +0900

    실습 완료

---
+++ Python/04_example.py
@@ -0,0 +1,40 @@
+# print() happy hacking!
+hphk = "해피해킹"
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+
+# 점심메뉴판!
+
+menus1 = "순남 시래기"
+menus2 = "양자강"
+menus3 = "20층..."
+
+print(menus1)
+print(menus2)
+print(menus3)
+
+# List
+menus = ["순남 시래기", "양자강", "20층..."]
+print(menus)
+print(menus[0])
+
+# 전화번호부 저장하기
+
+phone_nums = ["02-0216-1343", "02-1216-1343", "02-2216-1343"]
+
+print(phone_nums[0])
+
+# Dictionary
+
+dict_nums = { "순남 시래기" : "02-0216-1343", "양자강" : "02-1216-1343", "20층..." : "02-2216-1343"}
+
+# 오류 발생
+# print(dict_nums["헤헤"])
+# none 발생
+print(dict_nums.get("희희"))
+
+
