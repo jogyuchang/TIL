17bd42d4350de258c25f04af2950a798975fd4e9
jogyuchang <whrbckd23@naver.com>
2019-12-19 11:36:16 +0900

    Add Crawling

---
+++ Python/Crawling/exchang.py
@@ -0,0 +1,15 @@
+# 실습 환율 정보 가져오기
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = "https://finance.naver.com/marketindex/"
+response = requests.get(url).text
+soup = bs(response, 'html.parser')
+
+doller = soup.select_one("#exchangeList > li:nth-child(1) > a.head.usd > div > span.value")
+print(f"현재 원/달러 환율은 {doller.text} 입니다.")
+
+# 파일 저장      
+with open('test.txt','w',encoding='utf-8') as f:
+    f.write(f"현재 원/달러 환율은 {doller.text} 입니다.")
